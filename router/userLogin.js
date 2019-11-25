5a379ca145900ffbb29e19bd8183af5b42a314b4
DhanvarshaNS <dhanvarsha@neophrontech.com>
2019-11-25 11:21:20 +0530

    login updated and checked

--- router/userLogin.js
+++ router/userLogin.js
@@ -3,6 +3,12 @@ const userLoginController = require('./../controller/userLogin');
 
 router.post('/login', async (req, res) => {
     res.send(await userLoginController.login(req.body));
+    
+});
+
+router.post('/validateToken', async (req, res) => {
+    res.send(await userLoginController.validateToken(req.body));
+   
 });
 
 module.exports = router;
