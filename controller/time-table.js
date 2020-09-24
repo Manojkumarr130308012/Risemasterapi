55e15c3dc3e363de56b3ffa96075034459549c6e
new <santhiya@neophrontech.com>
2020-09-24 00:18:19 -0700

    make it better

--- controller/time-table.js
+++ controller/time-table.js
@@ -51,80 +51,10 @@ class timeTableController{
 	async fetchstaffperioddata(academicYear,staff,day){
 		try{
 			let response = await timeTableSchema.find({'academicYear':academicYear,'staff':staff,'day':day});
-
-			console.log("dddddddddd",response);
-
-			return await timeTableSchema.aggregate([
-				{
-
-					$match: {
-						sectionid: ObjectId(       ),							
-						staff: ObjectId(staff)								
-					
-					}
-				},				
-				
-				{
-					$lookup:
-					{
-						from: "week_days",
-						localField: "day",
-						foreignField: "_id",
-						as: "dayDetails"
-					}
-				},
-				{
-					$unwind : "$dayDetails"
-				},
-				{
-
-					$match: {
-						"dayDetails.day": day			
-					
-					}
-				},
-				{
-					$lookup:
-					{
-						from: "sections",
-						localField: "sectionid",
-						foreignField: "_id",
-						as: "sectionDetails"
-					}
-				},
-				{
-					$lookup:
-					{
-						from: "periods",
-						localField: "period",
-						foreignField: "_id",
-						as: "periodDetails"
-					}
-				},
-				{
-				$lookup:
-				{
-					from: "subject_details",
-					localField: "subject",
-					foreignField: "_id",
-					as: "subjectDetails"
-				}
-			},
-			{
-				$lookup:
-				{
-					from: "staff-profiles",
-					localField: "staff",
-					foreignField: "_id",
-					as: "staffDetails"
-				}
-			},
-				
-					
-
-			]);
-					
-			
+			return {
+				status: "success",
+				response: response
+			};
 			
 		} catch(error){
 			return {
